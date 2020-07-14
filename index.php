8526031db8fe0cf33a5e70585dfbf2a37165ea4f
RONGApp0 <66483852+RONGApp0@users.noreply.github.com>
2020-07-14 20:34:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php @header('HTTP/1.0 250 Error'); ?>
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+<head><title>IP错误-钟意博客-RONGApp官网</title>
+</head>
+<body bgcolor="white">
+<h1>ERROR</h1>
+<p>非法请求，您的ip地址为境外地址，为了您的使用安全，请使用中国大陆网络使用<hr/>Illegal request，Your IP address is outside China. For your safety, please use the network in China.
+如果判断有误，请联系我QQ977877731<br><hr/>
+<div style="background-color: #b7b1f2;">
+你当前的IP:<script src="//pv.sohu.com/cityjson?ie=utf-8"></script>
+                   <script type="text/javascript">
+                    document.write(returnCitySN["cip"])
+                   </script>
+
+
+</div>
+</body>
